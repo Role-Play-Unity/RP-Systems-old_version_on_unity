1ccb9cdb439171387d55e5621ca90d04c07225cb
Maxim360 <33699113+Maxim360Squad@users.noreply.github.com>
2021-05-09 01:40:38 +0300

    Create UI and Model. Create Data (for Rest Api)

---
+++ Passport-System/Assets/Frontend/Scripts/Game/Systems/Passport-System/PassportSystemMaster.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassportSystemMaster : MonoBehaviour
+{
+    public GameObject passpord3D;
+    public PassportSystemUI passportUI;
+    public Animation animm;
+    public AnimationClip anim_open;
+    public AnimationClip anim_close;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Passport-System/Assets/Frontend/Scripts/Game/Systems/Passport-System/PassportSystemUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PassportSystemUI : MonoBehaviour
+{
+    public Text passport_id_upper_part;
+    public Text passport_issued;
+    public Text date_of_issue;
+    public Text division_code; 
+    public Text signature_person_who_issued_passport;
+    public Text personal_code;
+    public Text personal_signature;
+    public Image division_logo;
+
+    public Text passport_id_lower_part;
+    public Image citizen_photo;
+    public Text lastname;
+    public Text firstname;
+    public Text patronymic;
+    public Text gender;
+    public Text date_of_birth;
+    public Text place_of_birth;
+
+    public Text bottom_text;
+}
