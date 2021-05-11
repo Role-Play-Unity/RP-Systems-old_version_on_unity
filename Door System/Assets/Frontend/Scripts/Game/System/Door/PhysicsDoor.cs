880e7c2d832ce80608d416f25a274d502173921e
Maxim360 <33699113+Maxim360Squad@users.noreply.github.com>
2021-05-11 23:43:53 +0300

    Door - Default and Physic Manager.

---
+++ Door System/Assets/Frontend/Scripts/Game/System/Door/PhysicsDoor.cs
@@ -0,0 +1,74 @@
+﻿// NULLcode Studio © 2015
+// null-code.ru
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(HingeJoint))]
+
+public class PhysicsDoor : MonoBehaviour {
+
+	public bool isOpen = false;
+	public float force = 50; // добавление силы при открытии замка и для закрытия двери
+	public float distance = 20;
+
+	private Rigidbody body;
+	private HingeJoint hinge;
+	private Vector3 pos;
+	private Quaternion rot;
+	private Transform target;
+
+	void Awake () 
+	{
+		pos = transform.localPosition;
+		rot = transform.localRotation;
+
+		hinge = GetComponent<HingeJoint>();
+		body = GetComponent<Rigidbody>();
+
+		if(!isOpen) body.isKinematic = true;
+
+		JointMotor motor = hinge.motor;
+		motor.force = force;
+		motor.targetVelocity = force;
+		motor.freeSpin = false;
+		hinge.motor = motor;
+
+		enabled = false;
+	}
+	
+	public void Invert(Transform player)
+	{
+		target = player;
+
+		if(!isOpen) 
+		{
+			body.isKinematic = false;
+			body.AddForce((transform.position - Vector3.forward).normalized * force);
+		}
+		else
+		{
+			hinge.useMotor = true;
+		}
+
+		isOpen = !isOpen;
+	}
+	
+	void Update ()
+	{
+		if(hinge.angle < 1 && hinge.useMotor)
+		{
+			hinge.useMotor = false;
+			body.isKinematic = true;
+			transform.localPosition = pos;
+			transform.localRotation = rot;
+			isOpen = false;
+		}
+
+		if(target)
+		{
+			float dis = Vector3.Distance(transform.position, target.position);
+			if(dis > distance) enabled = false;
+		}
+	}
+}
---
+++ Door System/Assets/Frontend/Scripts/Game/System/Door/PhysicsDoorComtrol.cs
@@ -0,0 +1,37 @@
+﻿// NULLcode Studio © 2015
+// null-code.ru
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+
+public class PhysicsDoorComtrol : MonoBehaviour {
+
+	public float distance = 5;
+	public string doorTag = "Door";
+	public KeyCode key = KeyCode.F;
+	private Camera cam;
+	
+	void Awake () 
+	{
+		cam = GetComponent<Camera>();
+	}
+	
+	void Update () 
+	{
+		if(Input.GetKeyDown(key))
+		{
+			RaycastHit hit;
+			Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+			if (Physics.Raycast(ray, out hit, distance))
+			{
+				if(hit.collider.tag == doorTag)
+				{
+					hit.transform.GetComponent<PhysicsDoor>().enabled = true;
+					hit.transform.GetComponent<PhysicsDoor>().Invert(transform);
+				}
+			}
+		}
+	}
+}
